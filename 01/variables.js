fc8f696bb0a70049e265e1fc0e93488187fb1eac
Abhi76311 <114270867+Abhi76311@users.noreply.github.com>
2025-01-20 21:52:34 +0000

    container config done

---
+++ 01/variables.js
@@ -0,0 +1,16 @@
+const id =12345
+let email ="Abhi@gmail.com"
+var password = 12345
+city = "Patna"
+// if we don't use any variable type in the variables then it will show its 'not defined'.But if we use any variaable type in the variable then we get get the value its 'undefined'.
+let state;
+
+email= "ram@gmail.com"
+password = "4567"
+city = "Ranchi"
+
+// id = 3456 (if any value is given in const it is fixed we can't cange the value of it)
+console.log(id);
+
+// if we want to print all data with a single line of code then we use table.
+console.table( [id, email, password, city, state])
