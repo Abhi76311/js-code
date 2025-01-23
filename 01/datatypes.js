4d65d723797526eeec476f043f70d61cb32cc27b
Abhi76311 <114270867+Abhi76311@users.noreply.github.com>
2025-01-23 13:33:38 +0000

    container config donne

---
+++ 01/datatypes.js
@@ -0,0 +1,18 @@
+// Datatypes
+
+// number = 2 to power of 53
+let age =20
+// bigint = big number more then number.
+// stringt = we write it with the help of ('') or (""). It stores the value of cherrectors.
+let Name = "Abhi"
+// boolean = true /falsa
+let isBoy = true
+// null = stand alone value it means there is nothing.
+// undefined =
+// symbol = unique
+
+console.log(typeof age);
+console.log(typeof Name);
+console.log(typeof isBoy);
+
+
